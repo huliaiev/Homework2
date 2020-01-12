e500f905fd158a24387b33e646c720ae2a663e0e
huliaiev <artyomgulyaev@ukr.net>
2020-01-12 13:10:14 +0200

    1,2,3

--- ChatClient/src/ua/kiev/prog/Main.java
+++ ChatClient/src/ua/kiev/prog/Main.java
@@ -1,5 +1,6 @@
 package ua.kiev.prog;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,49 +8,88 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         User user = new User();
 
-        System.out.println("в нашем чате нужна регистрация. Хотите зарегистрироваться ?");
-        String registration = scanner.nextLine();
-
-        if (registration.equalsIgnoreCase("Y")) {
-            user.registration();
-        }
-
-        if (registration.equalsIgnoreCase("N")) {
-            System.out.println("используйте ранее зарегистрированный логин");
-        }
-
         try {
+            user.addUser(new User("artem"));
+            user.addUser(new User("petro"));
+            user.addUser(new User("olga"));
+            user.addUser(new User("nastya"));
+            user.addUser(new User("dima"));
+
+            System.out.println("Если у вас нет логина, необходима регистрация. Хотите зарегистрироваться сейчас? (Y/N)");
+            String registration = scanner.nextLine();
+
+            while (!registration.equalsIgnoreCase("Y") && (!registration.equalsIgnoreCase("N") )) {
+                System.out.println("Повторите попытку ввода. Введите Y или N ");
+                registration = scanner.nextLine();
+            }
+
+            if (registration.equalsIgnoreCase("Y")) {
+                user.registration();
+            }
+
+            if (registration.equalsIgnoreCase("N")) {
+                String text1 = "Используйте ранее зарегистрированный логин";
+                System.out.println(text1);
+            }
+
             System.out.println("Enter your login: ");
-            String login = scanner.nextLine();
+            String login1 = scanner.nextLine();
 
+            System.out.println(user.searchByLogin(login1));
 
-            /*
-            Проверка зарегистрирован ли такой логин
+            if (user.searchByLogin(login1) == true)  {
+                System.out.println("Добро пожаловать в систему");
+            }
 
-            user.searchByLogin(login);
+            if (user.searchByLogin(login1) == false) {
+                System.out.println("Такого логина нет");
 
-             */
+                while (user.searchByLogin(login1) == false) {
+                    System.out.println("Повторите попытку ввода логина");
+                    login1 = scanner.nextLine();
+                }
+            }
 
             Thread th = new Thread(new GetThread());
             th.setDaemon(true);
             th.start();
 
             System.out.println("Enter your message: ");
+            System.out.println("Нажмите 1, чтобы получить список всех пользователей ");
+            System.out.println("Нажмите 2, чтобы написать сообщение пользователю ");
+
             while (true) {
                 String text = scanner.nextLine();
                 if (text.isEmpty()) break;
 
-                Message m = new Message(login, text);
+
+
+                Message m = new Message(login1, text);
                 int res = m.send(Utils.getURL() + "/add");
 
                 if (res != 200) { // 200 OK
                     System.out.println("HTTP error occured: " + res);
                     return;
                 }
+
+                //список всех пользователей
+                if (text.equals("1")) {
+                    user.showUsers();
+                }
+
+                //сообщение для пользователя
+                if (text.equals("2")) {
+                    user.privateMessages();
+                }
+
+                //чат комната
+                if (text.equals("3")) {
+                    user.chatRoom();
+                }
+
             }
         } catch (IOException ex) {
             ex.printStackTrace();
--- ChatClient/src/ua/kiev/prog/User.java
+++ ChatClient/src/ua/kiev/prog/User.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class User {
     String login;
     List<User> usersArray = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
     public User(String login) {
         this.login = login;
@@ -16,7 +17,6 @@ public class User {
         super();
     }
 
-
     public String getLogin() {
         return login;
     }
@@ -25,7 +25,7 @@ public class User {
         this.login = login;
     }
 
-
+    //метод добавления пользователей в Arraylist
     public void addUser(User user) {
         if (usersArray.size() < 100) {
             usersArray.add(user);
@@ -34,24 +34,81 @@ public class User {
         }
     }
 
+    //метод регистрации в чате
     public void registration() {
-        System.out.println("Введите логин");
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваш новый логин: ");
         String login = scanner.nextLine();
 
+        for (User element : usersArray) {
+            while (element.getLogin().equalsIgnoreCase(login)) {
+                System.out.println("Такой логин уже есть. Попробуйте другой");
+                login = scanner.nextLine();
+            }
+        }
         User user1 = new User(login);
         addUser(user1);
-        System.out.println("Спасибо за регистрацию на нашем чате");
+
     }
 
+    //метод проверки логина в базе
     public boolean searchByLogin(String login) {
         for (User element : usersArray) {
-            if (element.getLogin() == login) {
+            if (element.getLogin().equalsIgnoreCase(login)) {
                 return true;
             }
         }
         return false;
     }
 
+    //метод для получения списка всех пользователей
+    public void showUsers() {
+        for (User element : usersArray) {
+            System.out.println(element.getLogin());
+        }
+    }
 
+    //функция приватных сообщений
+    public void privateMessages() {
+        System.out.println("Введите логин прользователя, кому вы хотите написать");
+        String string2 = scanner.nextLine();
+
+        for (User element : usersArray) {
+            if (element.getLogin().equalsIgnoreCase(string2)) {
+                System.out.println("Такой пользователь есть");
+                break;
+            }
+
+        }
+        System.out.println("Введите текст сообщения для пользователя " + string2);
+        String string3 = scanner.nextLine();
+        System.out.println("Пользователь: " + string2 + " ,Вам сообщение: " + string3);
+    }
+
+    //чат комната
+    public void chatRoom() {
+        System.out.println("Введите логины пользователей, с которыми вы хотите начать общение");
+        String string3 = scanner.nextLine();
+
+        String[] arraysString = string3.split("[, ?.@]+");
+
+        for (String element : arraysString) {
+            System.out.println(element);
+        }
+
+       /*
+       
+       проверка есть ли такие имена в базе
+
+        */
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "login='" + login + '\'' +
+                ", usersArray=" + usersArray +
+                '}';
+    }
 }
